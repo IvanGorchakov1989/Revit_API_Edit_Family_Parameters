61abbd7e057a0bc8b66d8bcb1f048b5022b85859
Ivan Gorchakov <125120848+IvanGorchakov1989@users.noreply.github.com>
2023-10-02 19:44:28 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,11 @@
 # module main.py
 # -*- coding: utf-8 -*-
+
+import os
+import sys
+sys.path += [
+    os.path.dirname(os.path.realpath(__file__)),
+]
 from Forms import Window
 
 __window__.Close() # noqa
@@ -7,3 +13,4 @@ __window__.Close() # noqa
 if __name__ == '__main__':
     f = Window()
     f.ShowDialog()
+    
