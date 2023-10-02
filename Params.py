136e60e83282cf328111a94fdbf3c5b39fb63db2
Ivan Gorchakov <125120848+IvanGorchakov1989@users.noreply.github.com>
2023-10-02 16:23:06 +0300

    Update Params.py

--- Params.py
+++ Params.py
@@ -7,21 +7,6 @@ uiapp = __revit__                          # noqa
 app = __revit__.Application                # noqa
 doc = __revit__.ActiveUIDocument.Document  # noqa
 
-# def unit_converter(
-#         doc,
-#         value,
-#         to_internal=False,
-#         unit_type=None,
-#         number_of_digits=None):
-#     display_units = doc.GetUnits().GetFormatOptions(unit_type).DisplayUnits
-#     method = DB.UnitUtils.ConvertToInternalUnits if to_internal \
-#         else DB.UnitUtils.ConvertFromInternalUnits
-#     if number_of_digits is None:
-#         return method(value, display_units)
-#     elif number_of_digits > 0:
-#         return round(method(value, display_units), number_of_digits)
-#     return int(round(method(value, display_units), number_of_digits))
-
 def get_parameters(file):
     f_doc = app.OpenDocumentFile(
         DB.FilePath(file),
